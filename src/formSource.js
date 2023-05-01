f12fa04f3e5ef85a1671990bc70560569e118897
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2023-05-01 14:34:56 -0400

    df-Cupones Company fin

--- src/formSource.js
+++ src/formSource.js
@@ -1,227 +1,232 @@
 export const userInputs = [
-    {
-      id: "username",
-      label: "Username",
-      type: "text",
-      placeholder: "john_doe",
-    },
-    {
-      id: "displayName",
-      label: "Name and surname",
-      type: "text",
-      placeholder: "John Doe",
-    },
-    {
-      id: "email",
-      label: "Email",
-      type: "mail",
-      placeholder: "john_doe@gmail.com",
-    },
-    {
-      id: "phone",
-      label: "Phone",
-      type: "text",
-      placeholder: "+1 234 567 89",
-    },
-    {
-      id: "password",
-      label: "Password",
-      type: "password",
-    },
-    {
-      id: "address",
-      label: "Address",
-      type: "text",
-      placeholder: "Elton St. 216 NewYork",
-    },
-    {
-      id: "country",
-      label: "Country",
-      type: "text",
-      placeholder: "USA",
-    },
-  ];
-  
-  export const productInputs = [
-    {
-      id: "Nombre",
-      label: "Nombre",
-      type: "text",
-      placeholder: "Apple Macbook Pro",
-      maxlength: "40",
-    },
-    {
-      id: "Descuento",
-      label: "Descuento",
-      type: "number",
-      placeholder: "23%",
-      maxlength: "6",
-    },
-    {
-      id: "Codigocupon",
-      label: "CodigoCupon",
-      type: "text",
-      placeholder: "CuponesdMYcupones",
-      maxlength: "20",
-    },
-    {
-      id: "Fechahasta",
-      label: "Fecha Hasta",
-      type: "date",
-    },
-    {
-      id: "Fechadesde",
-      label: "Fecha Desde",
-      type: "date",
-    },
-    
-  ];
-  export const selectcate = [
-    {
-      key: "Destacados",
-      name: "Destacados",
-    },
-    {
-      key: "Panoramas",
-      name: "Panoramas",
-    },
-    {
-      key: "Promociones",
-      name: "Promociones",
-    },
-    {
-      key: "Sabores",
-      name: "Sabores",
-    },
-    {
-      key: "Servicios",
-      name: "Servicios",
-    },
-    {
-      key: "Shopping",
-      name: "Shopping",
-    },
-    {
-      key: "Salud y Belleza",
-      name: "Salud y Belleza",
-    },
-    {
-      key: "Viajes",
-      name: "Viajes",
-    },
-    
-  ];
-  export const selectsubcate = [
-    {
-      key: "Ofertas populares",
-      name: "Ofertas populares",
-    },
-    {
-      key: "Nuevas ofertas",
-      name: "Nuevas ofertas",
-    },
-    {
-      key: "Exclusivo de Mycupones",
-      name: "Exclusivo de Mycupones",
-    },
-    {
-      key: "Último Minuto, ¡date prisa!",
-      name: "Último Minuto, ¡date prisa!",
-    }
-    
-  ];
-  export const empresaInputs = [
-    {
-      id: "Nombre",
-      label: "Nombre de la Empresa",
-      type: "text",
-      placeholder: "Lenovo",
-      maxlength: "20",
-    },
-    {
-      id: "Telefono",
-      label: "Teléfono",
-      type: "text",
-      placeholder: "+56 9 22996030",
-      maxlength: "20",
-    },
-    {
-      id: "Sitioweb",
-      label: "Sitio web",
-      type: "text",
-      placeholder: "www.accionvenus.com",
-      maxlength: "20",
-    },
-    {
-      id: "Facebook",
-      label: "Facebook",
-      type: "text",
-      placeholder: "Facebook",
-      maxlength: "20",
-    },
-    {
-      id: "Instagram",
-      label: "Instagram",
-      type: "text",
-      placeholder: "Instagram",
-      maxlength: "20",
-    },
-    {
-      id: "Twitter",
-      label: "Twitter",
-      type: "text",
-      placeholder: "Twitter",
-      maxlength: "20",
-    },
-  
-    
-  ];
-  export const empresaInputsedit = [
-    {
-      id: "Nombre",
-      label: "Nombre de la Empresa",
-      type: "text",
-      placeholder: "Lenovo",
-      maxlength: "20",
-    },
-    {
-      id: "Telefono",
-      label: "Teléfono",
-      type: "text",
-      placeholder: "+56 9 22996030",
-      maxlength: "20",
-    },
-    {
-      id: "Sitioweb",
-      label: "Sitio web",
-      type: "text",
-      placeholder: "www.accionvenus.com",
-      maxlength: "20",
-    },
-    {
-      id: "Facebook",
-      label: "Facebook",
-      type: "text",
-      placeholder: "Facebook",
-      maxlength: "20",
-    },
-    {
-      id: "Instagram",
-      label: "Instagram",
-      type: "text",
-      placeholder: "Instagram",
-      maxlength: "20",
-    },
-    {
-      id: "Twitter",
-      label: "Twitter",
-      type: "text",
-      placeholder: "Twitter",
-      maxlength: "20",
-    },
-  
-    
-  ];
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "john_doe",
+  },
+  {
+    id: "displayName",
+    label: "Name and surname",
+    type: "text",
+    placeholder: "John Doe",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "mail",
+    placeholder: "john_doe@gmail.com",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+1 234 567 89",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+    placeholder: "Elton St. 216 NewYork",
+  },
+  {
+    id: "country",
+    label: "Country",
+    type: "text",
+    placeholder: "USA",
+  },
+];
 
-  
-  
+export const productInputs = [
+  {
+    id: "Nombre",
+    label: "Nombre",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+    maxlength: "60",
+  },
+  {
+    id: "Descuento",
+    label: "Descuento",
+    type: "number",
+    placeholder: "23%",
+    maxlength: "6",
+  },
+  {
+    id: "Sitioweb",
+    label: "Sitioweb",
+    type: "text",
+    placeholder: "https://www.7veinte.cl/7Veinte%",
+  },
+  {
+    id: "Codigocupon",
+    label: "CodigoCupon",
+    type: "text",
+    placeholder: "CuponesdMYcupones",
+    maxlength: "20",
+  },
+  {
+    id: "Fechahasta",
+    label: "Fecha Hasta",
+    type: "date",
+  },
+  {
+    id: "Fechadesde",
+    label: "Fecha Desde",
+    type: "date",
+  },
+
+];
+export const selectcate = [
+  {
+    key: "Destacados",
+    name: "Destacados",
+  },
+  {
+    key: "Panoramas",
+    name: "Panoramas",
+  },
+  {
+    key: "Promociones",
+    name: "Promociones",
+  },
+  {
+    key: "Sabores",
+    name: "Sabores",
+  },
+  {
+    key: "Servicios",
+    name: "Servicios",
+  },
+  {
+    key: "Shopping",
+    name: "Shopping",
+  },
+  {
+    key: "Salud y Belleza",
+    name: "Salud y Belleza",
+  },
+  {
+    key: "Viajes",
+    name: "Viajes",
+  },
+
+];
+export const selectsubcate = [
+  {
+    key: "Ofertas populares",
+    name: "Ofertas populares",
+  },
+  {
+    key: "Nuevas ofertas",
+    name: "Nuevas ofertas",
+  },
+  {
+    key: "Exclusivo de Mycupones",
+    name: "Exclusivo de Mycupones",
+  },
+  {
+    key: "Último Minuto, ¡date prisa!",
+    name: "Último Minuto, ¡date prisa!",
+  }
+
+];
+export const empresaInputs = [
+  {
+    id: "Nombre",
+    label: "Nombre de la Empresa",
+    type: "text",
+    placeholder: "Lenovo",
+    maxlength: "20",
+  },
+  {
+    id: "Telefono",
+    label: "Teléfono",
+    type: "text",
+    placeholder: "+56 9 22996030",
+    maxlength: "20",
+  },
+  {
+    id: "Sitioweb",
+    label: "Sitio web",
+    type: "text",
+    placeholder: "www.accionvenus.com",
+    maxlength: "20",
+  },
+  {
+    id: "Facebook",
+    label: "Facebook",
+    type: "text",
+    placeholder: "Facebook",
+    maxlength: "20",
+  },
+  {
+    id: "Instagram",
+    label: "Instagram",
+    type: "text",
+    placeholder: "Instagram",
+    maxlength: "20",
+  },
+  {
+    id: "Twitter",
+    label: "Twitter",
+    type: "text",
+    placeholder: "Twitter",
+    maxlength: "20",
+  },
+
+
+];
+export const empresaInputsedit = [
+  {
+    id: "Nombre",
+    label: "Nombre de la Empresa",
+    type: "text",
+    placeholder: "Lenovo",
+    maxlength: "20",
+  },
+  {
+    id: "Telefono",
+    label: "Teléfono",
+    type: "text",
+    placeholder: "+56 9 22996030",
+    maxlength: "20",
+  },
+  {
+    id: "Sitioweb",
+    label: "Sitio web",
+    type: "text",
+    placeholder: "www.accionvenus.com",
+    maxlength: "20",
+  },
+  {
+    id: "Facebook",
+    label: "Facebook",
+    type: "text",
+    placeholder: "Facebook",
+    maxlength: "20",
+  },
+  {
+    id: "Instagram",
+    label: "Instagram",
+    type: "text",
+    placeholder: "Instagram",
+    maxlength: "20",
+  },
+  {
+    id: "Twitter",
+    label: "Twitter",
+    type: "text",
+    placeholder: "Twitter",
+    maxlength: "20",
+  },
+
+
+];
+
+
--- src/pages/edit/singlempre/singlempre/Singlempre.jsx
+++ src/pages/edit/singlempre/singlempre/Singlempre.jsx
@@ -1,8 +1,9 @@
 import "./single.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { productsColumns } from "../../../../datatablesource";
 import Sidebar from "../../../../components/sidebar/Sidebar";
 import Navbar from "../../../../components/navbar/Navbar";
 import Chart from "../../../../components/chart/Chart";
-import List from "../../../../components/table/Table";
 import { useEffect, useState } from "react";
 import {
   collection,
@@ -17,6 +18,7 @@ import { Link } from "react-router-dom";
 
 const Single = () => {
   const [data, setData] = useState([]);
+  const [datas, setDatas] = useState([]);
   const [localid, setLocalid] = useState(window.localStorage.getItem('id'));
   const [localidar, setLocalidar] = useState({});
 
@@ -36,7 +38,7 @@ const Single = () => {
     // };
     // fetchData();
 
-    // LISTEN (REALTIME)
+    // LISTEN (REALTIME Empresa (Nombre == id ))
     const colref = collection(db, "empresa");
 
     const q = query(colref, where("Nombre", "==", localid));
@@ -49,18 +51,57 @@ const Single = () => {
       });
       setData(list);
       window.localStorage.setItem("ids", JSON.stringify(list));
-      console.log("sd",localidar);
+      console.log("sd", localidar);
     },
       (error) => {
         console.log(error);
       }
     );
+    // Obtener datos de la colección 'cupones' filtrados por el campo 'Empresa'
+    const colrefs = collection(db, "cupones");
+    const qs = query(colrefs, where("Empresa", "==", localid));
+    const unsubs = onSnapshot(qs, (snapshot) => {
+      const lists = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setDatas(lists);
+      console.log("Cupones", lists);
+    }, (error) => {
+      console.log(error);
+    });
 
 
     return () => {
       unsub();
+      unsubs();
     };
+
   }, []);
+
+
+  const setLocalStorageid = async (id) => {
+    try {
+      await setLocalid(id);
+      window.localStorage.setItem("id", id);
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to={`/products/productIds/`} style={{ textDecoration: "none" }} onClick={() => setLocalStorageid(params.id)}>
+              <div className="viewButton">View</div>
+            </Link>
+          </div>
+        );
+      },
+    },
+  ];
 
   return (
     <div className="single">
@@ -70,7 +111,7 @@ const Single = () => {
         <div className="top">
           <div className="left">
             <div className="editButton"><Link to="/empresa/editempresa/" className="link">Edit</Link></div>
-            <h1 className="title">Information</h1>
+            <h1 className="title">Information Company</h1>
             {data.map((data) => (
               <div className="item" key={data.toString()}>
                 <img
@@ -104,10 +145,23 @@ const Single = () => {
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
           </div>
         </div>
-        <div className="bottom">
-          <h1 className="title">Last Transactions</h1>
-          <List />
+        <div className="datatable">
+          <div className="datatableTitle">
+            Cupones Company
+            <Link to="/products/newpro" className="link">
+              Add New
+            </Link>
+          </div>
+          <DataGrid
+            className="datagrid"
+            rows={datas}
+            columns={productsColumns.concat(actionColumn)}
+            pageSize={9}
+            rowsPerPageOptions={[9]}
+            checkboxSelection
+          />
         </div>
+
       </div>
     </div>
   );
